dab197723aeef685b9d2a77f8e2bf1988b9620d3
Niko Caron <ncaron@e1z2r3p3.42.us.org>
2016-11-08 14:59:15 -0800

    ft_strncmp fix, now compares null character

--- srcs/ft_strncmp.c
+++ srcs/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: Niko <niko.caron90@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/02 22:53:23 by Niko              #+#    #+#             */
-/*   Updated: 2016/11/02 22:56:23 by Niko             ###   ########.fr       */
+/*   Updated: 2016/11/08 14:58:44 by Niko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,15 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t			i;
+	size_t			s1_len;
+	size_t			s2_len;
 	unsigned char	c1;
 	unsigned char	c2;
 
 	i = 0;
-	while (!s1[i] || !s2[i])
-		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-	while (s1[i] && s2[i] && i < n)
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	while (i <= s1_len && i <= s2_len && i < n)
 	{
 		c1 = (unsigned char)s1[i];
 		c2 = (unsigned char)s2[i];
