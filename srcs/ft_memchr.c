727ed936a601434b28ab0cffa533c501a3552224
Niko Caron <ncaron@e1z2r6p8.42.us.org>
2016-11-09 13:42:59 -0800

    ft_memchr fix

--- srcs/ft_memchr.c
+++ srcs/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: Niko <niko.caron90@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 15:00:58 by Niko              #+#    #+#             */
-/*   Updated: 2016/11/07 20:30:20 by Niko             ###   ########.fr       */
+/*   Updated: 2016/11/09 13:41:38 by Niko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	unsigned char *ss;
 
 	ss = (unsigned char*)s;
-	while (*ss++ && n--)
+	while (n && *ss != (unsigned char)c)
 	{
-		if (*ss == (unsigned char)c)
-			return (ss);
+		ss++;
+		n--;
 	}
-	if ((unsigned char)c == '\0')
-		return (ss);
-	return (NULL);
+	if (n == 0)
+		return (NULL);
+	return (ss);
 }
